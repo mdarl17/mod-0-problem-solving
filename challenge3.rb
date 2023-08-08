3c214714ba6f752e24b38238853ec2f85d4bbcc2
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-08 14:35:01 -0600

    Minor edits to challenge3.rb

--- challenge3.rb
+++ challenge3.rb
@@ -3,12 +3,20 @@
 # 1. Goal:
     # Define a method that takes a string argument and returns that string after removing all 's' (I included capital 'S' as well, to show my unserstanding of how string.delete method works)
 
-# Removing all 's' instances - uppercase included - in string argument ('str')
+# 2. Pseudocode:
+    # Define method, beginning with def/end keyword syntax.
+    # Name method 'remove_s', and include a paramater, str, for the string input needed as argument when remove_s function is called.
+    # Use string.delete method, passing in either lowercase (if only removing lowercase 's' characters, or both 'Ss' if removing ALL instances of 's', including uppercase instances)
+    # Test code by using 'puts' function before calling the 'remove_s' method with a well known Daffy Duck quote that immediately comnes to mind when thinking of a string argument that would include a lot of 's' instances.
+
+
+## Removing all 's' instances - uppercase included - in string argument ('str')
+
 def remove_s(str)
     str.delete("Ss")
 end
 
-# Removing only lowercase 's'instances in string argument ('str')
+## Removing only lowercase 's'instances in string argument ('str')
 
 # def remove_s(str)
 #     str.delete("s")

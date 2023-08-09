c573599e55b24c717b717302a9af74b399d1f855
Matt Darlington <matthewsdarlington@gmail.com>
2023-08-08 18:20:12 -0600

    Add code to get user input and proper tax liability re: filing single or jointly

--- challenge7.rb
+++ challenge7.rb
@@ -10,24 +10,51 @@
 # Once 2nd-lowest range is added to the conditional chain, add the last 'catch-all' condition for argument values less than or equal to the lowest tax bracket (10%) for wages totaling less than or equal to $10,275.
 # Call the method 'calculate_tax' with an integer object as the sole argument and print this value to the console using the 'puts' function.
 
+## BONUS
+# I added a feature to get input from the user about the type of return they are filing. They are prompted with a question asking if they are filing an individual return. This would probably be more common and makes sense to be the default.
+# Check only against "y/Y" or "n/N". Although I include capitalization in prompt format instruction, I did set the program up to accept either an upper or lowercase "Y" or "N". Any other keyboard input will cause the program to terminate, but not before an error message lets the user know where their issue most likely occurred (typo on 'Y/n').
+# Conditional flow will depend on what the user enters, "Y/y", "N/n", or something invalid. If "Y/y", the income argument will be tested against the conditionals immediately following the 'elsif single == "Y".downcase' statement, returning a tax liability for someone filing individually. Similarly, if "N/n" is entered and stored in the variable, 'single', income will first be evaluated against the conditions that follow 'if single == "N".downcase', returning a tax liability for a joint filer, which is less than that of an individual filer.
+
+# 3. Final Solution:
+
 def calculate_tax(income)
-    if income > 539900
-        (income * 0.37).round
-    elsif income > 215950
-        (income * 0.35).round
-    elsif income > 170050
-        (income * 0.32).round
-    elsif income > 89075
-        (income * 0.24).round
-    elsif income > 41775
-        (income * 0.22).round
-    elsif income > 10275
-        (income * 0.12).round
+    puts "Is this an individual return? (Y/n)"
+    single = gets.chomp
+    if single == "N".downcase
+        if income > 647850
+            (income * 0.37).round
+        elsif income > 431900
+            (income * 0.35).round
+        elsif income > 340100
+            (income * 0.32).round
+        elsif income > 178150
+            (income * 0.24).round
+        elsif income > 83550
+            (income * 0.22).round
+        elsif income > 20550
+            (income * 0.12).round
+        else
+            (income * 0.1).round
+        end
+    elsif single == "Y".downcase
+        if income > 539900
+            (income * 0.37).round
+        elsif income > 215950
+            (income * 0.35).round
+        elsif income > 170050
+            (income * 0.32).round
+        elsif income > 89075
+            (income * 0.24).round
+        elsif income > 41775
+            (income * 0.22).round
+        elsif income > 10275
+            (income * 0.12).round
+        else
+            (income * 0.1).round
+        end
     else
-        (income * 0.1).round
+        "Please try running the program again. Only enter Y or n when\nprompted about whether you are filing an individual return."
     end
 end
 
-puts calculate_tax(800)
-
-
+puts calculate_tax(179000)
